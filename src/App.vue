15debb3f12893417358ffd3e83645a58be324deb
superbowen <z.bowen66@gmail.com>
2017-04-24 10:32:50 +0800

    兼容主流分辨率

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
 
     <div class="bg" id="bg">
-      <img src="../static/bg.jpg" alt="a cool bg" width="1920" height="1638">
+      <img src="../static/bg.jpg" alt="a cool bg" width="1920" height="2664">
     </div>
 
     <swiper :options="swiperOption" class="swiper-box">
@@ -50,7 +50,26 @@
             this.CurrentIndex = swiper.activeIndex
 //            console.log(document.getElementById('bg'))
 //            console.log(this.$refs.bg)
-            document.getElementById('bg').style.top = -this.CurrentIndex * 130 - 300 + 'px'
+            if (window.innerWidth < 768) { // mobile
+              document.getElementById('bg').style.top = -this.CurrentIndex * 50 - 800 + 'px'
+              console.log(document.getElementById('bg').style.top)
+              return
+            } else if (window.innerWidth < 769) { // 768
+              document.getElementById('bg').style.top = -this.CurrentIndex * 90 + 'px'
+              console.log(document.getElementById('bg').style.top)
+              return
+            } else if (window.innerWidth < 1025) { // 1024
+              document.getElementById('bg').style.top = -this.CurrentIndex * 90 - 300 + 'px'
+              console.log(document.getElementById('bg').style.top)
+              return
+            } else if (window.innerWidth < 1367) { // 1366
+              document.getElementById('bg').style.top = -this.CurrentIndex * 90 - 200 + 'px'
+              console.log(document.getElementById('bg').style.top)
+              return
+            } else {
+              document.getElementById('bg').style.top = -this.CurrentIndex * 130 - 300 + 'px'
+              console.log(document.getElementById('bg').style.top)
+            }
           }
         }
       }
@@ -61,64 +80,93 @@
   }
 </script>
 
-<style>
-  html, body {
+<style type="text/stylus" lang='stylus' rel='stylesheet/stylus'>
+
+  ::-webkit-scrollbar
+    display: none;
+
+  html, body
     position: relative;
     height: 100%;
-  }
+    #app
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      height: 100%;
+      .swiper-box
+        width: 100%;
+        height: 100%;
+        .page
+          height: 100%;
+          padding: 40px;
+          font-size: 1rem;
+        .swiper-pagination
+          right: 20px !important;
+        .swiper-pagination-bullet
+          width: .7rem !important;
+          height: .7rem !important;
+          margin-top: 1rem !important;
+          transition: all .7s;
+          &.swiper-pagination-bullet-active
+            transform: scale(1.5);
+            transition: all .7s;
+            background: rgb(131, 252, 216) !important;
 
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    font-size: 10px;
-    height: 100%;
-    /*color: #fff;*/
-  }
+  @media only screen and (min-width: 1367px)
+  //destop
+    #bg
+      position: fixed;
+      top: -300px;
+      right: 0;
+      transition: all .7s
 
-  #bg {
-    position: fixed;
-    top: -300px;
-    right: 0;
-    transition: all .7s
-  }
-  @media only screen and (max-width:1200px) {
-    #bg {
-      transform: scale(1.5);
-      right: -100px;
-    }
-  }
+    html
+      font-size: 32px
 
-  ::-webkit-scrollbar {
-    display: none;
-  }
+  @media only screen and (max-width: 1366px) and (min-width: 1025px)
+  //1366
+    #bg
+      position: fixed
+      right: -350px
+      top: -200px
+      transition: all .7s
 
-  .swiper-box {
-    width: 100%;
-    height: 100%;
-  }
+    html
+      font-size: 32px
 
-  .swiper-pagination {
-    right: 20px !important;
-  }
+  @media only screen and (max-width: 1365px) and (min-width: 1024px)
+  //1024
+    #bg
+      position: fixed
+      right: -350px
+      top: -300px
+      transition: all .7s
 
-  .swiper-pagination-bullet {
-    width: 1.2rem !important;
-    height: 1.2rem !important;
-    margin-top: 1rem !important;
-    transition: all .7s;
-  }
+    html
+      font-size: 32px
 
-  .swiper-pagination-bullet-active {
-    transform: scale(1.5);
-    transition: all .7s;
-    background: rgb(131, 252, 216) !important;
-  }
+  @media only screen and (max-width: 1023px) and (min-width: 768px)
+  //768
+    #bg
+      position: fixed
+      right: -350px
+      top: -200px
+      transition: all .7s
 
-  .page {
-    height: 100%;
-    padding: 40px;
-    font-size: 3rem;
-  }
+    html
+      font-size: 32px
+
+  @media only screen and (max-width: 767px)
+  //mobile
+    #bg
+      transform: scale(0.5)
+      position: fixed
+      right: -700px
+      top: -800px
+      transition: all .7s
+
+    html
+      font-size: 20px
+
 
 </style>
